ff5103bbb204c1b685aa17d9ea1ab9a69fc1e4d3
Zongheng Yang <zongheng.y@gmail.com>
2017-11-29 21:17:12 -0800

    clang-format'd, add date time string to status bar

--- common/load.cc
+++ common/load.cc
@@ -21,10 +21,10 @@
 // Based on: github.com/freebsd/freebsd/blob/master/usr.bin/top/machine.c
 // Based on: Apple.cpp for load_string/mem_string and apple's documentation
 
-#include <sstream>
-#include <string>
-#include <stdlib.h> // getloadavg()
 #include <cmath> // floorf()
+#include <sstream>
+#include <stdlib.h> // getloadavg()
+#include <string>
 #include <sys/types.h>
 
 #include "cpu.h"
@@ -34,74 +34,53 @@
 #include "powerline.h"
 
 // Load Averages
-std::string load_string( bool use_colors,
-  bool use_powerline_left, bool use_powerline_right,
-  short num_averages )
-{
+std::string load_string(bool use_colors, bool use_powerline_left,
+                        bool use_powerline_right, short num_averages) {
   std::ostringstream ss;
   double averages[num_averages];
   // based on: opensource.apple.com/source/Libc/Libc-262/gen/getloadavg.c
 
-  if( num_averages <= 0 || num_averages > 3)
-  {
-    ss << (char) 0;
+  if (num_averages <= 0 || num_averages > 3) {
+    ss << (char)0;
     return ss.str();
   }
 
-  if( getloadavg( averages, num_averages ) < 0 )
-  {
+  if (getloadavg(averages, num_averages) < 0) {
     ss << " 0.00 0.00 0.00"; // couldn't get averages.
-  }
-  else
-  {
-    unsigned load_percent = static_cast<unsigned int>( averages[0] /
-        get_cpu_count() * 0.5f * 100.0f );
+  } else {
+    unsigned load_percent = static_cast<unsigned int>(
+        averages[0] / get_cpu_count() * 0.5f * 100.0f);
 
-    if( load_percent > 100 )
-    {
+    if (load_percent > 100) {
       load_percent = 100;
     }
-    if( use_colors )
-    {
-      if( use_powerline_right )
-      {
-        powerline( ss, load_lut[load_percent], POWERLINE_RIGHT );
-      }
-      else if( use_powerline_left )
-      {
-        powerline( ss, load_lut[load_percent], POWERLINE_LEFT );
-      }
-      else
-      {
-        powerline( ss, load_lut[load_percent], NONE );
+    if (use_colors) {
+      if (use_powerline_right) {
+        powerline(ss, load_lut[load_percent], POWERLINE_RIGHT);
+      } else if (use_powerline_left) {
+        powerline(ss, load_lut[load_percent], POWERLINE_LEFT);
+      } else {
+        powerline(ss, load_lut[load_percent], NONE);
       }
     }
 
-    ss << ' ';
-    for( int i = 0; i < num_averages; ++i )
-    {
+    // ss << ' ';
+    for (int i = 0; i < num_averages; ++i) {
       // Round to nearest, make sure this is only a 0.00 value not a 0.0000
-      float avg = floorf( static_cast<float>( averages[i] ) * 100 + 0.5 ) / 100;
+      float avg = floorf(static_cast<float>(averages[i]) * 100 + 0.5) / 100;
       // Don't print trailing whitespace for last element
-      if ( i == num_averages-1 )
-      {
+      if (i == num_averages - 1) {
         ss << avg;
-      }
-      else
-      {
+      } else {
         ss << avg << " ";
       }
     }
 
-    if( use_colors )
-    {
-      if( use_powerline_left )
-      {
-        powerline( ss, load_lut[load_percent], POWERLINE_LEFT, true );
-        powerline( ss, "#[fg=default,bg=default]", POWERLINE_LEFT );
-      }
-      else if( !use_powerline_right )
-      {
+    if (use_colors) {
+      if (use_powerline_left) {
+        powerline(ss, load_lut[load_percent], POWERLINE_LEFT, true);
+        powerline(ss, "#[fg=default,bg=default]", POWERLINE_LEFT);
+      } else if (!use_powerline_right) {
         ss << "#[fg=default,bg=default]";
       }
     }
@@ -109,4 +88,3 @@ std::string load_string( bool use_colors,
 
   return ss.str();
 }
-
